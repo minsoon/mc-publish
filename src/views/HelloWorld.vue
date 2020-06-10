04ae288eaf57f89275433a52c94f3760d0f7e196
mcsung <mcsung@thewc.co.kr>
2020-06-10 16:20:30 +0900

    Add VueProject

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="startBox">
+    <h1><v-btn>HelloWorld</v-btn></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.startBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
